b7808e488464d320480df8e8f5b2285402683a9c
Justin Clark Ong <justinclarkong@gmail.com>
2024-02-20 08:38:27 +0800

    ci4: implement newstutorial

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -7,7 +7,11 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
+use App\Controllers\News;
 use App\Controllers\Pages;
+
+$routes->get('news', [News::class, 'index']);           // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
