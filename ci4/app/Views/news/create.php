d8577fa4380d334be93a30f768c3b06ddbf28822
Justin Clark Ong <justinclarkong@gmail.com>
2024-02-29 09:44:58 +0800

    create.php: adjust theming

--- ci4/app/Views/news/create.php
+++ ci4/app/Views/news/create.php
@@ -1,20 +1,18 @@
-<h2><?= esc($title) ?></h2>
-
-<article>
-<?= session()->getFlashdata('error') ?>
-<?= validation_list_errors() ?>
-
-<form action="../news" method="post">
-    <?= csrf_field() ?>
-
-    <label for="title">Title</label>
-    <input type="input" name="title" value="<?= set_value('title') ?>">
-    <br>
-
-    <label for="body">Text</label>
-    <textarea name="body" cols="45" rows="4"><?= set_value('body') ?></textarea>
-    <br>
-
-    <input type="submit" name="submit" value="Create news item">
-</form>
+<article class="center">
+	<?= session()->getFlashdata('error') ?>
+	<?= validation_list_errors() ?>
+	
+	<form action="../news" method="post">
+	    <?= csrf_field() ?>
+	
+	    <label for="title"><h2>Title</h2></label>
+	    <input type="input" name="title" value="<?= set_value('title') ?>">
+	    <br>
+	
+	    <label for="body"><h2>Text</h2></label>
+	    <textarea name="body" cols="45" rows="4"><?= set_value('body') ?></textarea>
+	    <br><br>
+	
+	    <input type="submit" name="submit" value="Create news item">
+	</form>
 </article>
