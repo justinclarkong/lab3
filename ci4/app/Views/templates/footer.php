d6a1a231b0ae048e4bd4d2ca3d148a109e9772d8
Justin Clark Ong <justinclarkong@gmail.com>
2024-03-06 21:23:17 +0800

    footer.php: add guest book

--- ci4/app/Views/templates/footer.php
+++ ci4/app/Views/templates/footer.php
@@ -1,4 +1,4 @@
-<footer><a href=<?= base_url('resources') ?>>Resources</a><br>Source code available on <a href="https://github.com/justinclarkong/lab3" target="_blank">GitHub</a> &hearts;</footer>
+<footer><a href=<?= base_url('resources') ?>>Resources</a> | <a href=<?= base_url('guests') ?>>Guest Book</a><br>Source code available on <a href="https://github.com/justinclarkong/lab3" target="_blank">GitHub</a> &hearts;</footer>
 <script src="js/scripts.js"></script>
 </body>
 </html>
